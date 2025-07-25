6423831acf0f32c60fb984b504ee5112a0cd0777
Ramadhani-Yassin <yasynramah@gmail.com>
2025-07-25 22:42:22 +0300

    backend

---
+++ backEnd/src/main/java/com/smart/safais/SafaisApplication.java
@@ -0,0 +1,13 @@
+package com.smart.safais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafaisApplication.class, args);
+	}
+
+}
